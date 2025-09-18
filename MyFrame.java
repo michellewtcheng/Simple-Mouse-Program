0f3960e5add71254ccc0d60362a073b9ded9a291
Michelle Cheng <mwcheng@student.unimelb.edu.au>
2025-09-18 23:02:56 +1000

    Created Main and MyFrame. Set up frame object and mouse listener

---
+++ MyFrame.java
@@ -0,0 +1,60 @@
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements MouseListener{
+
+    int clickCounterNum, mouseLocX, mouseLocY;
+    boolean isMousePresent;
+    JLabel clickCounter, mousePresent;
+
+    public MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+
+        this.clickCounter = new JLabel();
+        clickCounter.setBounds(0, 0, 250, 100);
+        clickCounter.setBackground(Color.LIGHT_GRAY);
+        clickCounter.setOpaque(true);
+
+        this.mousePresent = new JLabel();
+        mousePresent.setBounds(0, 100, 250, 100);
+        mousePresent.setBackground(Color.orange);
+        mousePresent.setOpaque(true);
+
+        this.add(clickCounter);
+        this.add(mousePresent);
+        this.setVisible(true);
+
+        this.clickCounterNum = 0;
+        this.mouseLocX= 0;
+        this.mouseLocY = 0;
+        this.isMousePresent = false;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
